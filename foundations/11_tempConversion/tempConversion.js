481c747a6ec9bdbb193ba0a9ea86a19d2281bb14
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-01-10 23:04:07 +0800

    feat: completed exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convert = (temp - 32) / 1.8
+  return Math.round(convert * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let convert = (temp * 1.8) + 32
+  return Math.round(convert * 10) / 10
 };
 
 // Do not edit below this line
