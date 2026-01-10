cc63da56669501d1a90c1f4af07b35510c4ee935
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-01-10 21:48:30 +0800

    refactor: implemented a return statement without if/else statements

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
 // use rest method for multiple parameters
 const removeFromArray = function(arr, ...args) {
+    return arr.filter((item) => !args.includes(item))
     for (let i = 0; i < arr.length; i++){
         if (args.includes(arr[i])){
             arr.splice(i,1)
