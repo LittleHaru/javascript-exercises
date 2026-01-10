48c47dfc2580a83682ea768f3c126fb18aa9bf15
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-01-10 22:41:42 +0800

    feat: completed exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let total = 0
+    if (startNum < 0 || endNum < 0 || !Number.isInteger(startNum) || !Number.isInteger(endNum)) {
+        return "ERROR"
+    }
 
+    if (startNum > endNum) {
+        [startNum,endNum] = [endNum,startNum]
+    }    
+
+    for (let i = startNum; i <= endNum; i++) {total += i}
+    return total
 };
 
 // Do not edit below this line
