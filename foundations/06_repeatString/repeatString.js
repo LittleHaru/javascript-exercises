3bf45a03515c5e77f4a80a0ccc6a2a74e4888be3
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-01-10 20:50:56 +0800

    feat: completed exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+    let word = ""
+    if (repeat < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < repeat; i++){
+        word += string 
+    }
+    return word
 };
 
 // Do not edit below this line
