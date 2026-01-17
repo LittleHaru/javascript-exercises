9806c62d365aff526ff0c2fda8429093e49ef676
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-01-17 19:20:51 +0800

    feat: compelted exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,30 @@
-const add = function() {
-	
+const add = function(number1 , number2) {
+	return number1 + number2
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+  return number1 - number2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, val) => total += val, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc,val) => acc * val)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(number) {
+  let total = 1
+  for (let i = number; i > 0; i--){
+    total *= i
+  }
+  return total
+}
 
 // Do not edit below this line
 module.exports = {
