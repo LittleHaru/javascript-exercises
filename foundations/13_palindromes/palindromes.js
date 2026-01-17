982a9d37feeaeeb11cab8a707d2255abec7b8113
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-01-17 19:38:24 +0800

    feat: completed exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const alphabet = "1234567890qwertyuiopasdfghjklzxcvbnm"
+    const cleanedString = word
+    .toLowerCase()
+    .split('')
+    .filter((char) => alphabet.includes(char))
+    .join("")
+    const reversedString = cleanedString.split('').reverse().join('')
+    return cleanedString === reversedString
 };
 
 // Do not edit below this line
